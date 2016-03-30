bc6dbedb20b84a40bda2e074143a343125fccaef
Derek Caneja <derek@dispatch.me>
2016-03-30 18:32:41 -0400

    Update package to use versions from Meteor 1.3
    Use ES2015 conventions.
    Remove unused platform dependency.

--- geolocation.js
+++ geolocation.js
@@ -4,7 +4,7 @@ Geolocation = {};
  * The geolocation status.
  * @enum
  */
-var Status = Geolocation.Status = {
+const Status = Geolocation.Status = {
   ERROR: 0,
   ANDROID_GPS_OFF: 1,
   IOS_LOCATION_SERVICES_OFF: 2,
@@ -12,38 +12,36 @@ var Status = Geolocation.Status = {
   AVAILABLE: 4
 };
 
-var isAndroid = /android/g.test(navigator.userAgent.toLowerCase());
-var isIos = /(ipad|iphone|ipod)/g.test(navigator.userAgent.toLowerCase());
+const isAndroid = /android/g.test(navigator.userAgent.toLowerCase());
+const isIos = /(ipad|iphone|ipod)/g.test(navigator.userAgent.toLowerCase());
 
 /**
  * Check for geolocation access.
  * @param callback
  */
 Geolocation.checkAccess = function (callback) {
-  if (!callback) throw 'You must set a callback.';
+  if (!callback) throw new Error('You must set a callback.');
 
   if (isAndroid) {
     // support older app versions
     if (!window.GPSDetector) return callback(Status.ERROR);
 
-    window.GPSDetector.checkGPS(function (isEnabled) {
+    window.GPSDetector.checkGPS(isEnabled => {
       callback(isEnabled ? Status.AVAILABLE : Status.ANDROID_GPS_OFF);
-    }, function () {
+    }, () => {
       callback(Status.ERROR);
     });
-
-    return;
+  } else {
+    navigator.geolocation.getCurrentPosition(() => {
+      callback(Geolocation.Status.AVAILABLE);
+    }, error => {
+      if (error.message === 'Location services are disabled.') {
+        callback(Status.IOS_LOCATION_SERVICES_OFF);
+      } else if (isIos) {
+        callback(Status.IOS_APPLICATION_DOES_NOT_HAVE_PERMISSION);
+      } else {
+        callback(Status.ERROR);
+      }
+    });
   }
-
-  navigator.geolocation.getCurrentPosition(function () {
-    callback(Geolocation.Status.AVAILABLE);
-  }, function (error) {
-    if (error.message === 'Location services are disabled.') {
-      callback(Status.IOS_LOCATION_SERVICES_OFF);
-    } else if (isIos) {
-      callback(Status.IOS_APPLICATION_DOES_NOT_HAVE_PERMISSION);
-    } else {
-      callback(Status.ERROR);
-    }
-  });
 };
